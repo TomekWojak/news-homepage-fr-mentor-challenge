623743e1acf39f095648ac45a24e6a623ee659c1
Tomek <tomek.wojak796@gmail.com>
2025-03-05 00:29:06 +0100

    finished page

--- js/app.js
+++ js/app.js
@@ -2,6 +2,16 @@
 const burgerBtn = document.querySelector('.nav__burger-btn')
 const sideNavbar = document.querySelector('.nav__links-mobile')
 const overlay = document.querySelector('.overlay')
+const navLinks = document.querySelectorAll('.nav__links-mobile .nav__link')
+
+
+const closeNavbar = () => {
+    navLinks.forEach(link => link.addEventListener('click', () => {
+        sideNavbar.classList.remove('active')
+        overlay.classList.remove('active')
+        burgerBtn.setAttribute('aria-expanded', 'false')
+    }))
+}
 
 burgerBtn.addEventListener('click', () => {
     const isOpened = burgerBtn.getAttribute('aria-expanded')
@@ -13,4 +23,12 @@ burgerBtn.addEventListener('click', () => {
     }else {
         burgerBtn.setAttribute('aria-expanded', 'true')
     }
+
+    closeNavbar()
+})
+
+overlay.addEventListener('click', () => {
+    sideNavbar.classList.remove('active')
+    overlay.classList.remove('active')
+    burgerBtn.setAttribute('aria-expanded', 'false')
 })
